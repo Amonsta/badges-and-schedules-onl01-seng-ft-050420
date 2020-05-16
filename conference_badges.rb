44fbdc4b3aea386a2063d3cea3544ef5ddac0595
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-16 03:06:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,13 +10,9 @@ def batch_badge_creator(name)
 end
 
 def assign_rooms(name, room_number)
-  attendees == [badges]
-  badges.each do |speaker|
-  "Hello, #{name}! You'll be assigned to room #{room_number}!"
-  end
+  
 end
 
 def printer(attendees)
-  puts [badges]
-  puts [room_number]
+  
 end
