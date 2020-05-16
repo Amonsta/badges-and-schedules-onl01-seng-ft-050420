bfff73439240a215a624ff40cb5c6c6493297d92
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-16 03:08:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  names.collect { |name| badge.maker(name)}
+def batch_badge_creator(badges)
+  badges.collect { |name| badge_maker(name)}
   end
 end
 
